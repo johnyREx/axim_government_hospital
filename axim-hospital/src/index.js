818a3a154bb116af71622fdd534fcd52554b3a88
johnyREx <johnkwofie99@icloud.com>
2024-07-15 11:34:57 +0200

    lets see how this goes

--- axim-hospital/src/index.js
+++ axim-hospital/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import './assets/css/bootstrap.min.css';
+import './assets/css/font-awesome.min.css';
+import './assets/css/style.css';
+//import './assets/js/bootstrap.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
